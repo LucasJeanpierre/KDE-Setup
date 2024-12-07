d0a27e52ded70056bfc2ef1cc94a8f97f7fb0ed3
Lucas Jeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2024-12-07 14:34:48 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+sudo apt update
+sudo apt upgrade
+
+
+sudo apt install vim neovim curl neofetch qt5-style-kvantum
+
+
+# font
+curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Mononoki.zip
+unzip Mononoki.zip
+mkdir /usr/share/fonts/mononoki
+mv Mononoki* /usr/share/fonts/mononoki/
+fc-cache
+
+
+#starship
+curl -sS https://starship.rs/install.sh | sh
+
+echo "eval \"$(starship init zsh)\"" >> .zshrc
+mkdir -p ~/.config && touch ~/.config/starship.toml
+source .zshrc
