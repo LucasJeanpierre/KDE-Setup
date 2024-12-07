31f8d43c0e242b73dc985c69005ff6a1fbd903f4
Lucas Jeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2024-12-07 17:18:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt install zsh vim neovim curl neofetch qt5-style-kvantum latte-dock
 
 
 # font
-curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Mononoki.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Mononoki.zip
 sudo unzip Mononoki.zip
 sudo mkdir /usr/share/fonts/mononoki
 sudo mv Mononoki* /usr/share/fonts/mononoki/
