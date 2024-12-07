284c877654b577674644ec864f07ffc6672d89b5
Lucas Jeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2024-12-07 19:08:32 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
-sudo apt update
-sudo apt upgrade
+#!/bin/bash
 
+# Update and upgrade system
+sudo apt update && sudo apt upgrade -y
 
-sudo apt install zsh vim neovim curl neofetch qt5-style-kvantum latte-dock
+# Install essential packages
+sudo apt install -y zsh vim neovim curl neofetch qt5-style-kvantum
 
-
-# font
+# Install fonts (Mononoki for Nerd Fonts)
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Mononoki.zip
 sudo unzip Mononoki.zip
 sudo mkdir /usr/share/fonts/mononoki
@@ -15,12 +16,39 @@ fc-cache
 
 cd ~
 
-#starship
+# Install Starship prompt
 curl -sS https://starship.rs/install.sh | sh
-
-echo 'eval "$(starship init zsh)"' >> .zshrc
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 mkdir -p ~/.config && touch ~/.config/starship.toml
-source .zshrc
+source ~/.zshrc
 
-# https://store.kde.org/p/1905813
-# latte dock : edna
+
+# Set up Utterly Nord theme for KDE
+# Install Utterly Nord theme dependencies
+sudo apt install -y gtk2-engines-murrine gtk3-engines-unico
+
+# Download the Utterly Nord theme for Plasma
+git clone https://github.com/HimDek/Utterly-Nord-Plasma.git ~/.themes/utterly-nord
+
+# Set the Plasma theme
+kwriteconfig5 --file kwinrc ActiveTheme "utterly-nord"
+kwriteconfig5 --file kwinrc Theme "Breeze Dark"
+
+# Set GTK theme for applications
+gsettings set org.gnome.desktop.interface gtk-theme "utterly-nord"
+gsettings set org.gnome.desktop.interface icon-theme "utterly-nord"
+
+# Set Qt theme for KDE apps (using Kvantum)
+kwriteconfig5 --file kwinrc QtStyle "kvantum"
+kwriteconfig5 --file kvantumrc Theme "UtterlyNord"
+
+# Set the Plasma color scheme to Nord
+kwriteconfig5 --file plasma-rc ColorScheme "Nord"
+
+
+# Restart plasma to apply the theme changes
+kquitapp5 plasmashell
+kstart5 plasmashell
+
+# Finish setup
+echo "Utterly Nord theme applied successfully!"
